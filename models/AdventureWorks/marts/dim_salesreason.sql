b871e27781acdab85c6d57afb5975425b78918ef
gabrielamantesantos.14662@gmail.com <gabrielamantesantos.14662@gmail.com>
2021-11-10 03:47:29 +0000

    falta dim_salesorder

---
+++ models/AdventureWorks/marts/dim_salesreason.sql
@@ -0,0 +1,19 @@
+{{  config(materialized='table')  }}
+
+with
+    salesreason as (
+        select reasontype
+            , salesreasonid
+	        , name
+        from {{ ref('stg_salesreason') }}
+    )
+    , preorder as (
+        select salesreason.salesreasonid
+        , salesorderheader.salesorderid
+        , salesreason.reasontype
+        , salesreason.name
+        from {{ ref('stg_salesorderheadersalesreason') }} salesorderheader
+        left join salesreason on salesorderheader.salesreasonid = salesreason.salesreasonid
+    )
+
+select * from preorder
